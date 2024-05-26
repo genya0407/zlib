12d6dbf0d9e9753d403f881ab40edaa5241d2ee9
Yusuke Sangenya <longinus.eva@gmail.com>
2024-05-26 22:13:23 +0900

    Added adler32_combine and crc32

--- ext/zlib_ext/zlib.c
+++ ext/zlib_ext/zlib.c
@@ -4689,8 +4689,8 @@ Init_zlib_ext(void)
 
     rb_define_module_function(mZlib, "zlib_version", rb_zlib_version, 0);
 //    rb_define_module_function(mZlib, "adler32", rb_zlib_adler32, -1);
-    rb_define_module_function(mZlib, "adler32_combine", rb_zlib_adler32_combine, 3);
-    rb_define_module_function(mZlib, "crc32", rb_zlib_crc32, -1);
+//    rb_define_module_function(mZlib, "adler32_combine", rb_zlib_adler32_combine, 3);
+//    rb_define_module_function(mZlib, "crc32", rb_zlib_crc32, -1);
     rb_define_module_function(mZlib, "crc32_combine", rb_zlib_crc32_combine, 3);
     rb_define_module_function(mZlib, "crc_table", rb_zlib_crc_table, 0);
 
--- lib/zlib.rb
+++ lib/zlib.rb
@@ -26,6 +26,35 @@ module Zlib
     b %= 65521
     b * 65536 + a
   end
+
+  def adler32_combine(adler1, adler2, len2)
+    # https://github.com/madler/zlib/blob/0f51fb4933fc9ce18199cb2554dacea8033e7fd3/adler32.c#L138C1-L140C29
+    return 0xffffffff if len2 < 0
+
+    a1 = adler1 & 0xFFFF
+    b1 = (adler1 >> 16) & 0xFFFF
+    a2 = adler2 & 0xFFFF
+    b2 = (adler2 >> 16) & 0xFFFF
+
+    a = (a1 + a2 - 1) % 65521
+    b = (b1 + b2 + (a1 - 1) * len2) % 65521
+    b * 65536 + a
+  end
+
+  CRC_TABLE = Array.new(256) do |i|
+    8.times do
+      i = i.odd? ? (0xEDB88320 ^ (i >> 1)) : (i >> 1)
+    end
+    i
+  end
+
+  def crc32(str = '', c = 0)
+    c = ~c & 0xFFFFFFFF
+    str.each_byte do |byte|
+      c = CRC_TABLE[(c ^ byte) & 0xFF] ^ (c >> 8)
+    end
+    c ^ 0xFFFFFFFF
+  end
 end
 
 require_relative 'zlib_ext'
